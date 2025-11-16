c073fa4553975361a41f9bb3dc01bbb78dc47e48
Stiven hoxha <stivenhoxha007@gmail.com>
2025-11-16 21:03:21 +0100

    visualizzazione dei parametri tramite la chiamata asincrona

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // CHIAVE API
 const apiKey = "9d2792481e5495acb7fb660801c44533";
 // URL DELLA CHIAVE API
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?uniits=metric&q=roma";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?uniits=metric&q=tirana";
 
 // FUNZIONE ASINCRONA CON IL NOME CHECKWEATHER
 async function checkWeather() {
@@ -9,6 +9,14 @@ async function checkWeather() {
     const data = await response.json();
 
     console.log(data);
+
+    // VISUALIZZARE I PARAEMTRI NECESSARI SU HTML TRAMITE I DATI PRESI DALLA CHIAMATA ASYNC SOPRA INDICATA
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C"; // ARROTONDARE I GRADI
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+
 }
 
 // PER CHIAMARE LA FUNZIONE ASINCRONA SOPRA INDICATA, BISOGNA CHIAMARLA FUORI DALLE GRAFFE, COME MOSTRATO QUA
